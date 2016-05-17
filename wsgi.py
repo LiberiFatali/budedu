2a7b0dddd1f4d7550040e42d0414125928044cfe
Nguyen Minh Hieu <hieunp131@gmail.com>
2016-05-17 14:06:03 +0700

    *test setting up Flask

--- wsgi.py
+++ wsgi.py
@@ -12,25 +12,33 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
-def application(environ, start_response):
+#def application(environ, start_response):
 
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-	response_body = open(os.environ['OPENSHIFT_REPO_DIR'] + 'bud_onepage/index.html').read()
+    #ctype = 'text/plain'
+    #if environ['PATH_INFO'] == '/health':
+        #response_body = "1"
+    #elif environ['PATH_INFO'] == '/env':
+        #response_body = ['%s: %s' % (key, value)
+                    #for key, value in sorted(environ.items())]
+        #response_body = '\n'.join(response_body)
+    #else:
+        #ctype = 'text/html'
+	#response_body = open(os.environ['OPENSHIFT_REPO_DIR'] + 'bud_onepage/index.html').read()
 
 
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body]
+    #status = '200 OK'
+    #response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    ##
+    #start_response(status, response_headers)
+    #return [response_body]
+    
+
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+from flaskapp import app as application
 
 #
 # Below for testing only
@@ -39,4 +47,5 @@ if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
-    httpd.handle_request()
+    #httpd.handle_request()
+    httpd.serve_forever()
