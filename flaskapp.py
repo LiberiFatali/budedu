a6590a60ce779579ca30786c73444d2fd8f2349a
Nguyen Minh Hieu <hieunp131@gmail.com>
2016-05-25 09:54:08 +0700

    *modify flask path to handle requests to gallery, charity

--- flaskapp.py
+++ flaskapp.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path = "", static_folder = "static")
 
 @app.route('/')
-def hello_world():
+def bud():
     #return 'Hello World!'
     return render_template('index.html') 
+  
+@app.route('/gallery')
+def bud():    
+    return render_template('gallery.html') 
+  
+@app.route('/charity')
+def bud():    
+    return render_template('charity.html') 
 
 if __name__ == '__main__':
     app.run()
