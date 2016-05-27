371be88e6e5ebf8044cd7eea5c8a0a98a707e8c9
Nguyen Minh Hieu <hieunp131@gmail.com>
2016-05-27 13:20:50 +0700

    *Fixed menu not disappeared on mobile devices

--- static/js/main.js
+++ static/js/main.js
@@ -230,11 +230,4 @@ jQuery(function($) {'use strict';
 	}
 	google.maps.event.addDomListener(window, 'load', initialize_map);
 
-
-	// Close menu on mobile devices
-	$(".navbar-nav li a").click(function(event) {
-	  $(".navbar-collapse.in").collapse('hide');
-	});
-	
 });
-
