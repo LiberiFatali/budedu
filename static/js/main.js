764b2f1273c23d78a0415f075221727c5f3afe43
Nguyen Minh Hieu <hieunp131@gmail.com>
2016-06-06 16:01:57 +0700

    +menu item Gallery and Charity now link to separate pages

--- static/js/main.js
+++ static/js/main.js
@@ -31,7 +31,7 @@ jQuery(function($) {'use strict';
 	};
 
 	$('#tohash').on('click', function(){
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 35}, 1000);
 		return false;
 	});
 
