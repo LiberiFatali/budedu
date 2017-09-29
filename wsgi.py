5442d87a9d843148c6484e3217c842a57b2160c9
Nguyen Minh Hieu <hieunp131@gmail.com>
2017-09-29 10:05:48 +0700

    Modify wsgi for openshift 3

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-import os
+# import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+# virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+# virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+# try:
+#     execfile(virtualenv, dict(__file__=virtualenv))
+# except IOError:
+#     pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
@@ -44,8 +44,9 @@ from flaskapp import app as application
 # Below for testing only
 #
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
-    # Wait for a single request, serve it and quit.
-    #httpd.handle_request()
-    httpd.serve_forever()
+    # from wsgiref.simple_server import make_server
+    # httpd = make_server('localhost', 8051, application)
+    # # Wait for a single request, serve it and quit.
+    # #httpd.handle_request()
+    # httpd.serve_forever()
+    application.run()
